4ee51cac1a790251665bd8142faa729d03587ea8
matheus.nicolas <matheus.nicolas@LAB1-DCX-19.dcx.ufpb.br>
2016-10-17 09:43:26 -0300

    Alterei variácel e fiz um cálculo

--- src/br/ufpb/es2/Gabriel.java
+++ src/br/ufpb/es2/Gabriel.java
@@ -1,6 +1,14 @@
 package br.ufpb.es2;
 
-public class Gabriel {
-	int yow = 17;
 
+
+public class Gabriel {
+	public static void main(String[] args){
+		double variavel = 17;
+
+		double total = Math.pow(variavel, 5);
+		System.out.println(total);
+		
+	}
+	
 }
